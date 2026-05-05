f52e8893a4519e1afed2a40dbbccc23e7faf5518
nguyen ha trung <hatrungngn@gmail.com>
2026-05-05 23:54:23 +0700

    update

--- routes/orders.js
+++ routes/orders.js
@@ -6,24 +6,18 @@ const router = new Router({
   prefix: '/api/orders'
 });
 
-// Create a new order
 router.post('/', orderController.createOrder.bind(orderController));
 
-// Get all orders with filters
 router.get('/', orderController.getOrders.bind(orderController));
 
-// Get orders by schedule
 router.get('/schedule/:scheduleId', orderController.getOrdersBySchedule.bind(orderController));
 
-// Get orders by session
 router.get('/session/:sessionId', orderController.getOrdersBySession.bind(orderController));
 
-// Get orders by user (with optional scheduleId)
 router.get('/user/:userId', orderController.getOrdersByUser.bind(orderController));
 
 router.get('/:orderId', orderController.getOrderDetail.bind(orderController));
 
-// Update order status
 router.patch('/:orderId/status', orderController.updateOrderStatus.bind(orderController));
 
 module.exports = router;
